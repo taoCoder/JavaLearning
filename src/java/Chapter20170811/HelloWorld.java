b9484a83078251c3b2608b1af82a0eee28279db1
huangtao54 <huangtao8@jd.com>
2017-08-24 18:32:12 +0800

    test

--- src/java/Chapter20170811/HelloWorld.java
+++ src/java/Chapter20170811/HelloWorld.java
@@ -2,6 +2,7 @@ package Chapter20170811;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("吴会子是个笨蛋！！！！！！！");
+        System.out.println("Hello World");
+        System.exit(0);
     }
 }
