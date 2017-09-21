db3cc61590a910d3d754c454f4d4f3cb9e4358cb
wuhuizi <wuhuizi7@163.com>
2017-09-21 16:02:44 +0800

    提交

---
+++ src/java/Employee.java
@@ -0,0 +1,24 @@
+public class Employee {
+    String name;
+    int age;
+    String designation;
+    double salary;
+    public Employee(String name){
+        this.name=name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public void printEmployee(){
+        System.out.println("名字:"+name);
+        System.out.println("年龄："+age);
+        System.out.println("职位："+designation);
+        System.out.println("薪资："+salary);
+    }
+}
